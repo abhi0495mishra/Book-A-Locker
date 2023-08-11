6ded4a602a2bf04790adeb5a252fd84211d7776c
Abhishek Mishra <mishra.abhi0495@gmail.com>
2023-08-11 13:05:52 +0100

    payment changes

--- app/src/main/java/com/abhimishra/lockerbooking/payment_activity.java
+++ app/src/main/java/com/abhimishra/lockerbooking/payment_activity.java
@@ -1,5 +1,12 @@
 package com.abhimishra.lockerbooking;
 
+import static com.abhimishra.lockerbooking.Constants.PAYMENT_AMOUNT;
+import static com.abhimishra.lockerbooking.Constants.PAYMENT_MODE;
+import static com.abhimishra.lockerbooking.Constants.REF_ID_TO_SEND;
+import static com.abhimishra.lockerbooking.Constants.SELECTED_END_DATE_TO_SEND;
+import static com.abhimishra.lockerbooking.Constants.SELECTED_ITEM_TO_SEND;
+import static com.abhimishra.lockerbooking.Constants.SELECTED_START_DATE_TO_SEND;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,7 +18,12 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.abhimishra.lockerbooking.databases.DatabaseContract;
+
 import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class payment_activity extends AppCompatActivity {
 
@@ -20,6 +32,14 @@ public class payment_activity extends AppCompatActivity {
     private RadioButton payLaterBtn;
     private Button finalPayBtn;
     private TextView contactThroughGmail;
+
+    private String refIdText;
+
+    private String startDate;
+    private String endDate;
+    private String lockerSelected;
+
+    private String paymentAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +51,15 @@ public class payment_activity extends AppCompatActivity {
         payLaterBtn = findViewById(R.id.payLaterRadioButton);
         contactThroughGmail = findViewById(R.id.text_view_contactGmail);
         finalPayBtn = findViewById(R.id.finalPayButton);
+
+        //get the value of referene ID from Start/End Date page
+
+        Intent intent = getIntent();
+        refIdText = intent.getStringExtra(REF_ID_TO_SEND);
+        startDate = intent.getStringExtra(SELECTED_START_DATE_TO_SEND);
+        endDate = intent.getStringExtra(SELECTED_END_DATE_TO_SEND);
+        lockerSelected = intent.getStringExtra(SELECTED_ITEM_TO_SEND);
+        paymentAmount = intent.getStringExtra(PAYMENT_AMOUNT);
 
         finalPayBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,19 +83,28 @@ public class payment_activity extends AppCompatActivity {
 
     private void performPayment() {
 
+        Intent intent = new Intent(payment_activity.this, booking_confirmation_page.class);
+        intent.putExtra(REF_ID_TO_SEND, refIdText);
+        intent.putExtra(SELECTED_START_DATE_TO_SEND,startDate);
+        intent.putExtra(SELECTED_END_DATE_TO_SEND,endDate);
+        intent.putExtra(SELECTED_ITEM_TO_SEND,lockerSelected );
+        intent.putExtra(PAYMENT_AMOUNT,paymentAmount);
 
         if (creditDebitBtn.isChecked()) {
             // Simulate credit/debit card payment
             // Replace with your payment processing logic
+
             Toast.makeText(this, "Credit/Debit Card Payment Successful", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(payment_activity.this, booking_confirmation_page.class);
+            intent.putExtra(PAYMENT_MODE, "Credit/Debit Card");
             startActivity(intent);
 
         } else if (payLaterBtn.isChecked()) {
             // Simulate "Pay Later" payment
             // Replace with your payment processing logic
+
+            //Move to Next Activity
             Toast.makeText(this, "Pay Later Payment Successful", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(payment_activity.this, booking_confirmation_page.class);
+            intent.putExtra(PAYMENT_MODE, "Pay Later");
             startActivity(intent);
 
         } else {
