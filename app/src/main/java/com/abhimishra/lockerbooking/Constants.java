f66e02907f790573985642a62966faf01d08663f
Abhishek Mishra <mishra.abhi0495@gmail.com>
2023-08-11 13:15:33 +0100

    Add Constants changes

---
+++ app/src/main/java/com/abhimishra/lockerbooking/Constants.java
@@ -0,0 +1,18 @@
+package com.abhimishra.lockerbooking;
+
+public class Constants {
+
+    public static final String REF_ID_TO_SEND = "REF_ID_TO_SEND";
+    public static final String SELECTED_ITEM_TO_SEND = "SELECTED_ITEM_TO_SEND";
+    public static final String SELECTED_START_DATE_TO_SEND = "SELECTED_START_DATE_TO_SEND";
+    public static final String SELECTED_END_DATE_TO_SEND = "SELECTED_END_DATE_TO_SEND";
+    public static final String PAYMENT_MODE = "PAYMENT_MODE";
+
+    public static final String REFERENCE_ID = "REFERENCE_ID";
+    public static final String PAYMENT_AMOUNT = "PAYMENT_AMOUNT";
+
+
+
+
+
+}
